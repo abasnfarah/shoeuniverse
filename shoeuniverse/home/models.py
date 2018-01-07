e21278d31038d44ed84dfb950b455c064f6debdb
Mikhaila Friske <frisk028@umn.edu>
2018-01-07 14:09:27 -0600

    home

--- shoeuniverse/home/models.py
+++ shoeuniverse/home/models.py
@@ -1,9 +1,20 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.db import models
+from django.shortcuts import render
 
-from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.models import Page, Orderable
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
+class ShoeTile(models.Model):
+	class Meta:
+		verbose_name = "Tile"
+		verbose_name_plural = "Tiles"
 
 class HomePage(Page):
-    pass
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+    ]
