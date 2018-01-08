bafe063d00c8c0d316a64226c232826d3f7a492c
Mikhaila Friske <frisk028@umn.edu>
2018-01-07 22:31:13 -0600

    test image chooser

--- shoeuniverse/home/models.py
+++ shoeuniverse/home/models.py
@@ -11,11 +11,18 @@ from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from modelcluster.fields import ParentalKey
 
 class HomePage(Page):
-#    image = models.imageField(upload_to='uploaded_images', defaults='')
     body = RichTextField(blank=True)
+    banner = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
+        ImageChooserPanel('banner'),
     ]
 
 class ShoeHistoryPage(Page):
